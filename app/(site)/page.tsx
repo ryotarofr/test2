bc56f31c0069ec1ce7850c01970199c4372bfb49
ryotarofr <ryoryo.fr0608@icloud.com>
2023-07-19 13:39:25 +0900

    test

---
+++ app/(site)/page.tsx
@@ -0,0 +1,35 @@
+import getContents from '@/actions/getContents';
+import Header from '../../components/Header';
+import PageContent from './components/PageContent';
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const contents = await getContents()
+
+  return (
+    <div className=" bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1
+            className="
+            text-white 
+              text-3xl 
+              font-semibold
+            ">
+            Welcome back
+          </h1>
+        </div>
+      </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest Info
+          </h1>
+        </div>
+        <PageContent contents={contents} />
+      </div>
+    </div>
+  )
+}
+
